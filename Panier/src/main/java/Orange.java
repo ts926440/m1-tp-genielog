8b4eac78450ff9dbd66916b781df25e7934ab237
ts926440 <ts926440@MI119-01>
2017-09-28 08:40:56 +0200

    Creation class Orange

---
+++ Panier/src/main/java/Orange.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ts926440
+ */
+public class Orange {
+    private String origine;
+    private double prix;
+    
+    public Orange(){
+        origine="France";
+        prix=2.00;
+    }
+    
+    public double getPrix(){
+        return prix;
+    }
+    public String getOrigine(){
+        return origine;
+    }
+}
